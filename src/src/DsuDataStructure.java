48c5612cb61b2896c2e82994bf740e17655576c8
sumit  negi <sumitnegi@sumits-MacBook-Air.local>
2023-10-29 22:58:18 +0530

    Adding DSU Algo

---
+++ src/src/DsuDataStructure.java
@@ -0,0 +1,23 @@
+public class DsuDataStructure {
+    int[] parent;
+    int[] rank;
+    public int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public boolean union(int x, int y) {
+        int xr = find(x), yr = find(y);
+        if (xr == yr) {
+            return false;
+        } else if (rank[xr] < rank[yr]) {
+            parent[xr] = yr;
+        } else if (rank[xr] > rank[yr]) {
+            parent[yr] = xr;
+        } else {
+            parent[yr] = xr;
+            rank[xr]++;
+        }
+        return true;
+    }
+}
